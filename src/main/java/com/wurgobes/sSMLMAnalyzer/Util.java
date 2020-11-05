21aa3722710c40eb3845b24d2c1e4d290d5e421f
dr_njitram <martijn.gobes@gmail.com>
2020-11-05 15:06:35 +0100

    Initial data Processing is finished

--- src/main/java/com/wurgobes/sSMLMAnalyzer/Util.java
+++ src/main/java/com/wurgobes/sSMLMAnalyzer/Util.java
@@ -8,15 +8,17 @@ import java.util.Arrays;
 import static org.jblas.MatrixFunctions.sqrt;
 import static org.jblas.MatrixFunctions.atan;
 
-//Very much not a perfect solution
-//https://en.wikipedia.org/wiki/Atan2
+
 
 public class Util {
 
     public static FloatMatrix atan2(FloatMatrix x, FloatMatrix y){
-        FloatMatrix result = new FloatMatrix(x.getRows());
+        //Very much not a perfect solution
+        //https://en.wikipedia.org/wiki/Atan2
+        x.assertSameSize(y);
+        FloatMatrix result = new FloatMatrix(x.rows, x.columns);
 
-        FloatMatrix intermediate = sqrt(x.mul(x).add(y.mul(y)));
+        FloatMatrix intermediate = Distance(x, y);
 
         FloatMatrix domainOne = atan(y.div(intermediate.add(x))).mul(2);
         FloatMatrix domainTwo = atan(intermediate.sub(x).div(y)).mul(2);
@@ -26,68 +28,11 @@ public class Util {
         for(int index : x.lt(0).and(y.eq(0)).findIndices()) result.put(index, (float) Math.PI);
         for(int index : x.eq(0).and(y.eq(0)).findIndices()) result.put(index, Float.NaN);
 
+
         return result;
     }
 
-    public static FloatMatrix AnglesBetweenPoints(FloatMatrix X, FloatMatrix Y){
-        if (X.rows != Y.rows)
-            throw new IllegalArgumentException(
-                    "Matrices must have same number of rows");
-
-        int dataPoints = X.rows;
-        FloatMatrix angles = new FloatMatrix(dataPoints, dataPoints);
-
-        FloatMatrix anglesX = new FloatMatrix(dataPoints, dataPoints);
-        FloatMatrix anglesY = new FloatMatrix(dataPoints, dataPoints);
-
-
-        //System.out.println(angles);
-        //System.out.println(angles.getRows());
-        //System.out.println(angles.getColumns());
-
-        for(int point = 0; point < dataPoints; point++){
-            float x = X.get(point);
-            float y = Y.get(point);
-
-            FloatMatrix temp_x = X.sub(x);
-            FloatMatrix temp_y = Y.sub(y);
-
-            angles.putColumn(point, atan2(temp_x, temp_y));
-
-            anglesX.putColumn(point, X.sub(x));
-            anglesY.putColumn(point, Y.sub(y));
-        }
-
-
-        return angles;
-    }
-
-    public static FloatMatrix Distance(FloatMatrix X, FloatMatrix Y){
-        return sqrt(X.mul(X).addi(Y.mul(Y)));
-    }
-
-    public static FloatMatrix Distances(FloatMatrix X, FloatMatrix Y) {
-        if (X.rows != Y.rows)
-            throw new IllegalArgumentException(
-                    "Matrices must have same number of rows");
-
-        int dataPoints = X.rows;
-
-        FloatMatrix distancesX = new FloatMatrix(dataPoints, dataPoints);
-        FloatMatrix distancesY = new FloatMatrix(dataPoints, dataPoints);
-
-        for(int point = 0; point < dataPoints; point++){
-            float x = X.get(point);
-            float y = Y.get(point);
-
-            distancesX.putColumn(point, X.sub(x));
-            distancesY.putColumn(point, Y.sub(y));
-        }
-
-
-
-        return Distance(distancesX, distancesY);
-    }
+    public static FloatMatrix Distance(FloatMatrix X, FloatMatrix Y){ return sqrt(X.mul(X).addi(Y.mul(Y))); }
 
     public static FloatMatrix makeSubstractedMatrix(FloatMatrix A){
         if(A.columns > 1)
@@ -99,9 +44,10 @@ public class Util {
 
         for(int point = 0; point < dataPoints; point++){
             float a = A.get(point);
-            subtracted.putColumn(point, A.sub(a));
-
+            subtracted.putRow(point, A.sub(a));
         }
         return subtracted;
     }
+
+
 }
--- src/main/java/com/wurgobes/sSMLMAnalyzer/sSMLMA.java
+++ src/main/java/com/wurgobes/sSMLMAnalyzer/sSMLMA.java
@@ -8,56 +8,49 @@ import org.scijava.plugin.Parameter;
 
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.jblas.FloatMatrix;
 
 import static com.wurgobes.sSMLMAnalyzer.Util.*;
 import static com.wurgobes.sSMLMAnalyzer.levenshtein.getTheClosestMatch;
-import static org.jblas.Geometry.pairwiseSquaredDistances;
+
 
 
 public class sSMLMA implements PlugIn {
 
     private static final String COMMA_DELIMITER = ",";
 
+
     private final int[] unit_decades = {0, 0, 0, -2, -3, -6, -9, -10, -12, -15};
 
-    private final String[] possible_options = {"id", "frame", "x", "y", "z", "intensity", "offset", "bkgstd", "sigma1", "sigma2"};
+    private final String[] possible_options = {"id", "frame", "x", "y", "z", "intensity", "offset", "bkgstd", "sigma1", "sigma2", "uncertainty", "detections", "chi"};
     private final String[] unit_prefixes = {"null", "photons", "m", "cm", "mm", "um", "nm", "ang", "pm", "fm"};
 
     @Parameter
-    private String filePath = "C:\\Users\\Martijn\\Desktop\\Thesis2020\\SpectralData\\small.csv";
+    private final String filePath = "F:\\ThesisData\\output\\output1_merged_filter2.csv";
 
     @Parameter
-    private final boolean saveSCV = false;
+    private static final String CSV_FILE_NAME = "C:\\Users\\Martijn\\Desktop\\Thesis2020\\SpectralData\\own_output.csv";
 
     @Parameter
-    private final double[] angRange = {-0.04 * Math.PI, 0.04 * Math.PI};
+    private final boolean saveSCV = true;
 
     @Parameter
-    private final int[] distRange = {2500, 4500};
+    private final float[] angRange = {(float) (-0.04 * Math.PI), (float)(0.04 * Math.PI)};
 
+    @Parameter
+    private final float[] distRange = {1700, 2500}; //default was {2500, 4500}
 
-    private FloatMatrix getRecordFromLine(String line, int size) {
-        FloatMatrix values = new FloatMatrix(1, size);
-        try (Scanner rowScanner = new Scanner(line)) {
-            rowScanner.useDelimiter(COMMA_DELIMITER);
-            int i = 0;
-            while (rowScanner.hasNextFloat()) {
-                values.put(i, rowScanner.nextFloat());
-                i++;
-            }
-        }
-        return values;
-    }
 
     @Override
     public void run(String arg) {
@@ -83,6 +76,7 @@ public class sSMLMA implements PlugIn {
         assert floatMatrix != null;
 
 
+
         int[] revOptionsIndices = new int[possible_options.length];
         int[] unitsIndices = new int[collumns.size()];
 
@@ -106,7 +100,9 @@ public class sSMLMA implements PlugIn {
         System.out.println("Loading CSV took " + String.format("%.3f", csvTime/1000000000) + " s");
         double processingTime = System.nanoTime();
 
+        int id = 0;
 
+        FloatMatrix finalPossibilities = new FloatMatrix(0, 10);
         for(int frame = 1; frame <= frames; frame++){
             IJ.showProgress(frame, frames);
 
@@ -117,20 +113,121 @@ public class sSMLMA implements PlugIn {
             FloatMatrix subtractedX = makeSubstractedMatrix(frameData.getColumn(1));
             FloatMatrix subtractedY = makeSubstractedMatrix(frameData.getColumn(2));
 
-            FloatMatrix distances = Distances(frameData.getColumn(1), frameData.getColumn(2));
-            FloatMatrix angles = AnglesBetweenPoints(frameData.getColumn(1), frameData.getColumn(2));
+            FloatMatrix distances = Distance(subtractedX, subtractedY);
+            FloatMatrix angles = atan2(subtractedX, subtractedY);
 
-            System.out.println(angles.getColumn(0));
-            System.exit(0);
+
+            int[] correctAngleAndDistance = distances.gt(distRange[0]).and(distances.lt(distRange[1]))
+                    .and(angles.gt(angRange[0])).and(angles.lt(angRange[1])).findIndices();
+
+
+            if(correctAngleAndDistance.length > 1){
+
+                FloatMatrix possibilities = new FloatMatrix(correctAngleAndDistance.length, 10);
+
+                for(int i = 0; i < correctAngleAndDistance.length; i++) {
+                    int index = correctAngleAndDistance[i];
+                    possibilities.putRow(i, new FloatMatrix(1, 10,
+                            id++,                                                           //0
+                            frame,                                                          //1
+                            Math.floorDiv(index, distances.rows),                           //2
+                            frameData.get(index / distances.rows, 1),    //3
+                            frameData.get(index / distances.rows, 2),    //4
+                            index % distances.rows,                                         //5
+                            frameData.get(index % distances.rows, 1),    //6
+                            frameData.get(index % distances.rows, 2),    //7
+                            distances.get(index),                                           //8
+                            angles.get(index)                                               //9
+                    ));
+                }
+                FloatMatrix connectedIds = possibilities.getColumn(5);
+                List<Integer> toSkip = new ArrayList<>();
+                for(int i = 0; i < possibilities.rows; i++){
+                    if(!toSkip.contains(i)){
+                        FloatMatrix identicalIds = connectedIds.eq(connectedIds.get(i));
+                        float identicalIdSum = identicalIds.sum();
+                        if(identicalIdSum > 1.0f){
+
+                            float cumX = possibilities.get(i, 3);
+                            float cumY = possibilities.get(i, 4);
+
+                            for(int index2 : identicalIds.findIndices()) {
+                                if(index2 != i) {
+                                    cumX += possibilities.get(index2, 3);
+                                    cumY += possibilities.get(index2, 4);
+                                    toSkip.add(index2);
+                                }
+                            }
+
+                            FloatMatrix temp_arr = possibilities.getRow(i).dup();
+                            temp_arr.put(3, cumX/identicalIdSum);
+                            temp_arr.put(4, cumY/identicalIdSum);
+
+                            finalPossibilities = FloatMatrix.concatVertically(finalPossibilities, temp_arr);
+
+                        } else {
+                            finalPossibilities = FloatMatrix.concatVertically(finalPossibilities, possibilities.getRow(i));
+                        }
+                    }
+                }
+
+            } else if(correctAngleAndDistance.length > 0){
+                int index = correctAngleAndDistance[0];
+                finalPossibilities = FloatMatrix.concatVertically(finalPossibilities, new FloatMatrix(1, 10,
+                        id++,
+                        frame,
+                        Math.floorDiv(index, distances.rows),
+                        frameData.get( index/distances.rows, 1),
+                        frameData.get( index/distances.rows, 2),
+                        index%distances.rows,
+                        frameData.get( index%distances.rows, 1),
+                        frameData.get( index%distances.rows, 2),
+                        distances.get(index),
+                        angles.get(index)));
+            }
+        }
+
+        //Fix ids
+        id = 0;
+        String[] Header = {
+                "id","frame",
+                "start index",
+                "start x [" + unit_prefixes[unitsIndices[revOptionsIndices[2]]] + "]",
+                "start y [" + unit_prefixes[unitsIndices[revOptionsIndices[3]]] + "]",
+                "end index",
+                "end x [" + unit_prefixes[unitsIndices[revOptionsIndices[2]]] + "]",
+                "end y [" + unit_prefixes[unitsIndices[revOptionsIndices[3]]] + "]",
+                "distance [" + (unit_prefixes[unitsIndices[revOptionsIndices[2]]].equals(unit_prefixes[unitsIndices[revOptionsIndices[3]]]) ? unit_prefixes[unitsIndices[revOptionsIndices[2]]]:("(" + unit_prefixes[unitsIndices[revOptionsIndices[2]]] + "*" +unit_prefixes[unitsIndices[revOptionsIndices[2]]] + ")^½"))+ "]",
+                "angle"
+        };
+        System.out.println(Arrays.toString(Header));
+        for(int i = 0; i < finalPossibilities.rows; i++){
+            finalPossibilities.put(i, 0, id++);
+            System.out.println(finalPossibilities.getRow(i));
         }
 
         processingTime = System.nanoTime() - processingTime;
-        System.out.println("Processing data took " + String.format("%.3f", processingTime/1000000000) + " s");
+        System.out.println("\nProcessing data took " + String.format("%.3f", processingTime/1000000000) + " s");
 
 
 
+        if(saveSCV) SaveCSV(finalPossibilities, Header);
 
     }
+
+    public void SaveCSV(FloatMatrix data, String[] Headers)  {
+        File csvOutputFile = new File(CSV_FILE_NAME);
+        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+                pw.println(String.join(",", Headers));
+                pw.println(data.toString("%f", "", "",", ", "\n"));
+        } catch(IOException error) {
+            System.out.println("Could not save CSV.");
+            error.printStackTrace();
+        }
+
+    }
+
+
 
     public static void main(String[] args) {
         new ImageJ();
